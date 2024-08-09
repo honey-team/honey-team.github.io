b028e6e13119ec909118f1c6aa8a9f01e9632ada
bleudev <95937737+bleudev@users.noreply.github.com>
2024-08-09 22:26:02 +0000

    Deploying to public from @ honey-team/honey-team.github.io@64ad8ca80382a11ae3d1b428d92e89661b9b8971 🚀

---
+++ _next/static/cuvdULbJ05ly3h6XZFM3A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/40b99f720cc1307b.css","static/chunks/pages/index-3e7d6426de55c21d.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/gEeQnsLmDVTMOJyaNqhnp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/40b99f720cc1307b.css","static/chunks/pages/index-5b03ddbef4cb1e41.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
