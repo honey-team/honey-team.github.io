48768938f98c9979c6e9445778031428b03ee5d2
bleudev <95937737+bleudev@users.noreply.github.com>
2024-08-09 22:45:41 +0000

    Deploying to public from @ honey-team/honey-team.github.io@cf86dc570af510c7611cf79906ce04f44944356f 🚀

---
+++ _next/static/KoqcKU2cNg-2n5B5Um6qC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/40b99f720cc1307b.css","static/chunks/pages/index-04b286fdbc654c76.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
