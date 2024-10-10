711064af8602d1585cd17998bfdbaa210082fc01
bleudev <spdd5724@gmail.com>
2024-10-10 19:39:35 +0300

    ProjectPageCard: final

--- src/components/ProjectMembersAvatars/ProjectMembersAvatars.jsx
+++ src/components/ProjectMembersAvatars/ProjectMembersAvatars.jsx
@@ -1,13 +1,14 @@
 import styles from "./ProjectMemberAvatars.module.css";
+import cn from "classnames";
 
-export default function ProjectMembersAvatars({ members, with_text = false }) {
+export default function ProjectMembersAvatars({ members, with_text = false, classNames = [] }) {
     // members: ['gh': string]
 
     if (with_text)
         var _id = styles[`avtext${members.length < 3 ? members.length : 3}`];
 
     return (
-        <div className={styles["avatars_row"]}>  
+        <div className={cn(styles["avatars_row"], ...classNames)}>  
             {members.length != 0 && (
                 <>
                     {members.map(
--- src/components/ProjectPageCard/ProjectPageCard.jsx
+++ src/components/ProjectPageCard/ProjectPageCard.jsx
@@ -5,16 +5,8 @@ import ProjectMembersAvatars from "../ProjectMembersAvatars/ProjectMembersAvatar
 import MemberSocials from "../MemberSocials/MemberSocials";
 
 export default function ProjectPageCard({project, members, ...props}) {
-    // const fac = new FastAverageColor();
-
     const image_url = project.image ? Route(project.image) : Route('/images/projects/default.png');
     console.log(image_url)
-
-    // average(image_url, (err, color) => {
-    //     if (err) throw err;
-    //     var [red, green, blue, alpha] = color;
-    //     avcolor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
-    // });
 
     var { avcolor } = useImageColors(image_url, {
         generateGradient: false
@@ -22,20 +14,10 @@ export default function ProjectPageCard({project, members, ...props}) {
     if (!avcolor) avcolor = '#272727';
 
     let members_ghs = [];
-
     members.forEach((member) => {
         if (member)
             members_ghs.push(member?.gh);
-    })
-
-    // let socials = project.socials;
-    // socials = {
-    //     gh: project.gh,
-    //     ...socials
-    // };
-
-    console.log(members);
-    console.log(members_ghs);
+    });
 
     return (
         <div className={styles["card"]} style={{background: `linear-gradient(to right, ${avcolor} 0%, #272727 100%)`}}>
@@ -43,8 +25,7 @@ export default function ProjectPageCard({project, members, ...props}) {
                 <img src={image_url} className={styles["pimage"]}/>
                 <div>
                     <h1 className={styles["pname"]}>{project.gh}</h1>
-                    {/* <ProjectMembersAvatars members={["bleudev", "moontr3", "mbutsk"]} with_text={true}/> */}
-                    <ProjectMembersAvatars members={members_ghs} with_text={true}/>
+                    <ProjectMembersAvatars members={members_ghs} with_text={true} classNames={[styles["pmembers"]]}/>
                     <p className={styles["pdesc"]}>{project.description ? project.description : "Нет описания..."}</p>
                     <div className={styles["psocials"]}>
                         <MemberSocials socials={{gh: `honey-team/${project.gh}` , ...project.socials}} height={40}/>
